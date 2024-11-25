8d00de3022780c65db012330010d653338b06c79
UaHig <golosnyyigor@gmail.com>
2024-11-25 23:20:29 +0100

    fix: configuration script

--- src/components/BotpressChat.js
+++ src/components/BotpressChat.js
@@ -17,8 +17,8 @@ const BotpressChat = () => {
 
     // После загрузки inject.js инициализируем бот
     injectScript.onload = () => {
-      if (window.botpressWebChat) {
-        window.botpressWebChat.init({
+      if (window.botpress) {
+        window.botpress.init({
           botId: "cb1336c0-89fb-462e-9d97-a3360d2cee69", // Укажите ваш Bot ID
           hostUrl: "https://cdn.botpress.cloud/webchat/v2.2/inject.js", // Например, http://localhost:3000 или https://your-domain.com
           customStyle: {
@@ -27,6 +27,10 @@ const BotpressChat = () => {
           },
           enableReset: true,
           showPoweredBy: false,
+          showTimestamp: true,
+          closeOnEscape: true,
+          disableNotificationSound: false,
+          showCloseButton: true,
         });
       } else {
         console.error("Botpress WebChat не загружен. Проверьте inject.js.");
